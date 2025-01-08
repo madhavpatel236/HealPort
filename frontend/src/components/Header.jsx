b0ee9b68722013d2199cc7094177990fee0e2a21
Madhav Karavadiya <madhavpatel236@gmail.com>
2025-01-08 22:47:53 +0530

    intialize the header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+// in this file we will create a header component in which we have a photo poster in the home page below the navbar.
+
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
