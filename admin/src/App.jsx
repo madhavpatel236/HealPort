4a1684a0d4b68d3913b84783a0f8a096eeb34f2e
Madhav Karavadiya <madhavpatel236@gmail.com>
2025-02-16 10:17:04 +0530

    Make some small changes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { useState } from "react";
 import "./App.css";
 import Login from "./pages/Login";
 import { ToastContainer, toast } from "react-toastify";
-import store from "./store/store";
-import { Provider, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard";
+import AllApointment from "./pages/Admin/AllApointment";
+import AddDoctor from "./pages/Admin/AddDoctor";
+import DocotrList from "./pages/Admin/DoctorList";
 
 function App() {
   const token = useSelector((store) => store.adminToken);
@@ -13,6 +18,19 @@ function App() {
   return token ? (
     <div className="bg-[#F8F9FD]">
       <ToastContainer />
+      <Navbar />
+      <div className="flex items-start">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/all-apointment" element={<AllApointment />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/doctor-list" element={<DocotrList></DocotrList>} />
+
+
+        </Routes>
+      </div>
     </div>
   ) : (
     <>
